4c13077fd33308bbb9a641eb8216f567e231653c
Tóth László <toth.laszlo@adalon.hu>
2017-10-11 13:12:50 +0200

    Added Tomcat9x adapter.

--- src/main/java/hudson/plugins/deploy/CargoContainerAdapter.java
+++ src/main/java/hudson/plugins/deploy/CargoContainerAdapter.java
@@ -1,19 +1,9 @@
 package hudson.plugins.deploy;
 
-import hudson.EnvVars;
-import hudson.FilePath;
-import hudson.Launcher;
-import hudson.Util;
-import hudson.model.BuildListener;
-import hudson.model.AbstractBuild;
-import hudson.remoting.VirtualChannel;
-import hudson.util.VariableResolver;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 
-import jenkins.MasterToSlaveFileCallable;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.cargo.container.Container;
@@ -30,6 +20,16 @@ import org.codehaus.cargo.generic.configuration.DefaultConfigurationFactory;
 import org.codehaus.cargo.generic.deployer.DefaultDeployerFactory;
 import org.codehaus.cargo.generic.deployer.DeployerFactory;
 
+import hudson.EnvVars;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.Util;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
+import hudson.util.VariableResolver;
+import jenkins.MasterToSlaveFileCallable;
+
 /**
  * Provides container-specific glue code.
  *
@@ -41,112 +41,116 @@ import org.codehaus.cargo.generic.deployer.DeployerFactory;
  */
 public abstract class CargoContainerAdapter extends ContainerAdapter implements Serializable {
 
-    /**
-     * Returns the container ID used by Cargo.
-     *
-     * @return
-     */
-    protected abstract String getContainerId();
+	/**
+	 * Returns the container ID used by Cargo.
+	 *
+	 * @return
+	 */
+	protected abstract String getContainerId();
 
-    /**
-     * Fills in the {@link Configuration} object.
-     *
-     * @param config
-     */
-    protected abstract void configure(Configuration config, EnvVars envVars, VariableResolver<String> resolver);
+	/**
+	 * Fills in the {@link Configuration} object.
+	 *
+	 * @param config
+	 */
+	protected abstract void configure(Configuration config, EnvVars envVars, VariableResolver<String> resolver);
 
-    protected Container getContainer(ConfigurationFactory configFactory, ContainerFactory containerFactory, String id, EnvVars envVars, VariableResolver<String> resolver) {
-        Configuration config = configFactory.createConfiguration(id, ContainerType.REMOTE, ConfigurationType.RUNTIME);
-        configure(config, envVars, resolver);
-        return containerFactory.createContainer(id, ContainerType.REMOTE, config);
-    }
+	protected Container getContainer(ConfigurationFactory configFactory, ContainerFactory containerFactory, String id,
+			EnvVars envVars, VariableResolver<String> resolver) {
+		Configuration config = configFactory.createConfiguration(id, ContainerType.REMOTE, ConfigurationType.RUNTIME);
+		configure(config, envVars, resolver);
+		return containerFactory.createContainer(id, ContainerType.REMOTE, config);
+	}
 
-    protected void deploy(DeployerFactory deployerFactory, final BuildListener listener, Container container, File f, String contextPath) {
-        Deployer deployer = deployerFactory.createDeployer(container);
+	protected void deploy(DeployerFactory deployerFactory, final BuildListener listener, Container container, File f,
+			String contextPath) {
+		Deployer deployer = deployerFactory.createDeployer(container);
 
-        listener.getLogger().println("Deploying " + f + " to container " + container.getName() + " with context " + contextPath);
-        deployer.setLogger(new LoggerImpl(listener.getLogger()));
+		listener.getLogger()
+				.println("Deploying " + f + " to container " + container.getName() + " with context " + contextPath);
+		deployer.setLogger(new LoggerImpl(listener.getLogger()));
 
+		String extension = FilenameUtils.getExtension(f.getAbsolutePath());
+		if ("WAR".equalsIgnoreCase(extension)) {
+			WAR war = createWAR(f);
+			if (!StringUtils.isEmpty(contextPath)) {
+				war.setContext(contextPath);
+			}
+			deployer.redeploy(war);
+		} else if ("EAR".equalsIgnoreCase(extension)) {
+			EAR ear = createEAR(f);
+			deployer.redeploy(ear);
+		} else {
+			throw new RuntimeException("Extension File Error.");
+		}
+	}
 
-        String extension = FilenameUtils.getExtension(f.getAbsolutePath());
-        if ("WAR".equalsIgnoreCase(extension)) {
-            WAR war = createWAR(f);
-            if (!StringUtils.isEmpty(contextPath)) {
-                war.setContext(contextPath);
-            }
-            deployer.redeploy(war);
-        } else if ("EAR".equalsIgnoreCase(extension)) {
-            EAR ear = createEAR(f);
-            deployer.redeploy(ear);
-        } else {
-            throw new RuntimeException("Extension File Error.");
-        }
-    }
+	/**
+	 * Creates a Deployable object WAR from the given file object.
+	 *
+	 * @param deployableFile
+	 *            The deployable file to create the Deployable from.
+	 * @return A Deployable object.
+	 */
+	protected WAR createWAR(File deployableFile) {
+		return new WAR(deployableFile.getAbsolutePath());
+	}
 
-    /**
-     * Creates a Deployable object WAR from the given file object.
-     *
-     * @param deployableFile The deployable file to create the Deployable from.
-     * @return A Deployable object.
-     */
-    protected WAR createWAR(File deployableFile) {
-        return new WAR(deployableFile.getAbsolutePath());
-    }
+	protected String expandVariable(EnvVars envVars, VariableResolver<String> resolver, String variable) {
+		String temp = envVars.expand(variable);
+		return Util.replaceMacro(envVars.expand(variable), resolver);
+	}
 
-    protected String expandVariable(EnvVars envVars, VariableResolver<String> resolver, String variable) {
-        String temp = envVars.expand(variable);
-        return Util.replaceMacro(envVars.expand(variable), resolver);
-    }
+	/**
+	 * Creates a Deployable object EAR from the given file object.
+	 *
+	 * @param deployableFile
+	 *            The deployable file to create the Deployable from.
+	 * @return A Deployable object.
+	 */
+	protected EAR createEAR(File deployableFile) {
+		return new EAR(deployableFile.getAbsolutePath());
+	}
 
+	public boolean redeploy(FilePath war, final String contextPath, final AbstractBuild<?, ?> build, Launcher launcher,
+			final BuildListener listener) throws IOException, InterruptedException {
+		return war.act(new DeployCallable(this, getContainerId(), build.getEnvironment(listener), listener, contextPath));
+	}
 
-    /**
-     * Creates a Deployable object EAR from the given file object.
-     *
-     * @param deployableFile The deployable file to create the Deployable from.
-     * @return A Deployable object.
-     */
-    protected EAR createEAR(File deployableFile) {
-        return new EAR(deployableFile.getAbsolutePath());
-    }
+	public static class DeployCallable extends MasterToSlaveFileCallable<Boolean> {
 
-    public boolean redeploy(FilePath war, final String contextPath, final AbstractBuild<?, ?> build, Launcher launcher,
-                            final BuildListener listener) throws IOException, InterruptedException {
-        return war.act(new DeployCallable(this, getContainerId(), build.getEnvironment(listener), listener, contextPath));
-    }
+		private CargoContainerAdapter adapter;
+		private String containerId;
+		private BuildListener listener;
+		private String contextPath;
+		private EnvVars envVars;
 
-    public static class DeployCallable extends MasterToSlaveFileCallable<Boolean> {
+		public DeployCallable(CargoContainerAdapter adapter, String containerId, EnvVars envVars,
+				BuildListener listener, String contextPath) {
+			this.adapter = adapter;
+			this.containerId = containerId;
+			this.envVars = envVars;
+			this.listener = listener;
+			this.contextPath = contextPath;
+		}
 
-        private CargoContainerAdapter adapter;
-        private String containerId;
-        private BuildListener listener;
-        private String contextPath;
-        private EnvVars envVars;
+		public Boolean invoke(File f, VirtualChannel channel) throws IOException {
+			if (!f.exists()) {
+				listener.error(Messages.DeployPublisher_NoSuchFile(f));
+				return true;
+			}
 
-        public DeployCallable (CargoContainerAdapter adapter, String containerId, EnvVars envVars,
-                               BuildListener listener, String contextPath) {
-            this.adapter = adapter;
-            this.containerId = containerId;
-            this.envVars = envVars;
-            this.listener = listener;
-            this.contextPath = contextPath;
-        }
+			ClassLoader cl = getClass().getClassLoader();
+			DeployerFactory deployerFactory = new DefaultDeployerFactory(cl);
+			ConfigurationFactory configFactory = new DefaultConfigurationFactory(cl);
+			ContainerFactory containerFactory = new DefaultContainerFactory(cl);
 
-        public Boolean invoke(File f, VirtualChannel channel) throws IOException {
-            if (!f.exists()) {
-                listener.error(Messages.DeployPublisher_NoSuchFile(f));
-                return true;
-            }
+			VariableResolver<String> resolver = new VariableResolver.ByMap<String>(envVars);
+			Container container = adapter.getContainer(configFactory, containerFactory, containerId, envVars, resolver);
+			adapter.deploy(deployerFactory, listener, container, f,
+					Util.replaceMacro(envVars.expand(contextPath), resolver));
 
-            ClassLoader cl = getClass().getClassLoader();
-            DeployerFactory deployerFactory = new DefaultDeployerFactory(cl);
-            ConfigurationFactory configFactory = new DefaultConfigurationFactory(cl);
-            ContainerFactory containerFactory = new DefaultContainerFactory(cl);
-
-            VariableResolver<String> resolver = new VariableResolver.ByMap<String>(envVars);
-            Container container = adapter.getContainer(configFactory, containerFactory, containerId, envVars, resolver);
-            adapter.deploy(deployerFactory, listener, container, f, Util.replaceMacro(envVars.expand(contextPath), resolver));
-
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
---
+++ src/main/java/hudson/plugins/deploy/tomcat/Tomcat9xAdapter.java
@@ -0,0 +1,60 @@
+package hudson.plugins.deploy.tomcat;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.codehaus.cargo.container.configuration.Configuration;
+import org.codehaus.cargo.container.property.RemotePropertySet;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.plugins.deploy.ContainerAdapterDescriptor;
+import hudson.util.VariableResolver;
+
+/**
+ * Tomcat 9.x
+ *
+ * @author laslie
+ */
+public class Tomcat9xAdapter extends TomcatAdapter {
+
+	/**
+	 * Tomcat 8 support
+	 *
+	 * @param url
+	 *            Tomcat server location (for example: http://localhost:8080)
+	 * @param credentialsId
+	 *            tomcat manager username password credentials
+	 */
+	@DataBoundConstructor
+	public Tomcat9xAdapter(String url, String credentialsId) {
+		super(url, credentialsId);
+	}
+
+	public void configure(Configuration config, EnvVars envVars, VariableResolver<String> resolver) {
+		super.configure(config, envVars, resolver);
+		try {
+			URL _url = new URL(expandVariable(envVars, resolver, url) + "/manager/text");
+			config.setProperty(RemotePropertySet.URI, _url.toExternalForm());
+		} catch (MalformedURLException e) {
+			throw new AssertionError(e);
+		}
+	}
+
+	/**
+	 * Tomcat Cargo containerId
+	 * 
+	 * @return tomcat9x
+	 */
+	public String getContainerId() {
+		return "tomcat9x";
+	}
+
+	@Extension
+	public static final class DescriptorImpl extends ContainerAdapterDescriptor {
+		public String getDisplayName() {
+			return "Tomcat 9.x";
+		}
+	}
+}
